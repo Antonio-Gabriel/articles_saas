0c7b161f4f5942fc7b7e5cc8083acc68a384db0f
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-11-27 00:54:38 +0100

    chore: adding article entity

---
+++ src/domain/entities/article.py
@@ -0,0 +1,49 @@
+# pylint: disable=too-many-instance-attributes
+from datetime import datetime
+from dataclasses import dataclass
+from typing import Optional, Type, Union
+
+from ..value_objects import UniqueId
+from ..adapter import ValidatorAdapter
+from ..validators import article_validator_schema
+
+
+@dataclass
+class ArticleProps:
+    """article entities"""
+    title: str
+    url: str
+    summary: str
+    featured: bool
+    image_url: str
+    news_site: str
+    published_at: datetime
+    updated_at: datetime
+
+
+class Article:
+    """article entity"""
+
+    def __init__(self, props: Type[ArticleProps], _id: Optional[str] = None) -> None:
+        self.__article_id = UniqueId.new(_id)
+        self.props = props
+
+    @property
+    def get_id(self) -> str:
+        """get id"""
+        return self.__article_id
+
+    def validate(self) -> Union[str, dict]:
+        """validate entities"""
+        is_valid, errs = ValidatorAdapter.validate(
+            {
+                "title": self.props.title,
+                "summary": self.props.summary,
+                "news_site": self.props.news_site,
+                "featured": self.props.featured,
+                "url": self.props.url,
+                "image_url": self.props.image_url,
+            }, article_validator_schema
+        )
+
+        return is_valid, errs
