6023b795632146b5a40ab0af0caa00f542a91606
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-11-27 08:30:33 +0100

    feat: adding create articles usecase

---
+++ src/application/usecases/__init__.py
@@ -0,0 +1,1 @@
+from .create_article_usecase import CreateArticleUsecase
---
+++ src/application/usecases/create_article_usecase.py
@@ -0,0 +1,54 @@
+# pylint: disable=too-few-public-methods
+from dataclasses import dataclass
+from typing import Type, Optional, Union
+
+from ..factory import LogFactory
+from ..dtos import ArticleRequestDto
+from ..repositories import ArticleRepositoryInterface
+
+from ...domain.entities import Article
+from ...domain.common.result import Result
+from ...domain.adapter.article_adapter import ArticleAdapter
+
+
+@dataclass
+class ResultStructure:
+    """structure of result"""
+    message: Optional[Union[str, dict, Article]]
+    status: int
+
+
+class CreateArticleUsecase:
+    """create article usecase"""
+
+    def __init__(self, articles_repository: Type[ArticleRepositoryInterface]) -> None:
+        self.__articles_repository = articles_repository
+
+    async def execute(self, article_data: Type[ArticleRequestDto]) -> Result[ResultStructure]:
+        """execute create article usecase"""
+
+        article = ArticleAdapter.create(
+            article_data.title, article_data.summary, article_data.news_site,
+            article_data.featured, article_data.image_url, article_data.url
+        )
+
+        is_valid, err = article.validate()
+
+        if is_valid is False:
+            log = LogFactory.create()
+            log.set_log(ResultStructure(
+                message=err,
+                status=400
+            ), "error")
+
+            return Result[ResultStructure].fail(ResultStructure(
+                message=err,
+                status=400
+            ))
+
+        resp = await self.__articles_repository.create(article=article)
+        if resp:
+            return Result[ResultStructure].ok(ResultStructure(
+                message=article,
+                status=200
+            ))
