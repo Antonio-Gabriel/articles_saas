a8e96fadaf851171ecc8e1b92efa12f13e0ffde8
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-11-27 07:58:04 +0100

    chore: adding entity representation

--- src/domain/entities/article.py
+++ src/domain/entities/article.py
@@ -47,3 +47,12 @@ class Article:
         )
 
         return is_valid, errs
+
+    def __repr__(self) -> str:
+        return f"""
+            Article(
+                title={self.props.title}, summary={self.props.summary}, news_site={self.props.news_site},
+                featured={self.props.featured}, url={self.props.url}, image_url={self.props.image_url}, 
+                published_at={self.props.published_at}, updated_at={self.props.updated_at}
+            )
+        """
